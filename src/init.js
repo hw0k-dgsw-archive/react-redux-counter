3d69afce401d9560d490712e3cf394eed5a1887b
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-04-22 13:54:06 +0900

    Init

---
+++ src/init.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+import "normalize.css";
+
+import "./styles/global.css";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
---
+++ src/styles/base.js
@@ -0,0 +1,15 @@
+export default {
+  color: {
+    primaryColor: "#1abc9c",
+    primaryColorDarken: "#27ae60",
+    white: "#ffffff",
+    red: "#e74c3c",
+    redDarken: "#c0392b"
+  },
+  size: {
+    base: "16px",
+    small: "0.8rem",
+    medium: "1rem",
+    big: "1.2rem"
+  }
+};
